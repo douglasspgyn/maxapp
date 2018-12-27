f1a7f9473920bbfda614470f80607c362a099eaf
Douglas <douglasoarespereira@gmail.com>
2018-12-27 14:45:31 -0200

    - Implement a basic MainActivity;

---
+++ app/src/main/java/douglasspgyn/com/github/maxapp/extension/Context.kt
@@ -0,0 +1,14 @@
+package douglasspgyn.com.github.maxapp.extension
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+fun Context.isNetworkConnected(): Boolean {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+    return connectivityManager?.let {
+        val netInfo = it.activeNetworkInfo
+        netInfo?.let {
+            it.isConnected
+        } ?: false
+    } ?: false
+}
---
+++ app/src/main/java/douglasspgyn/com/github/maxapp/ui/main/MainActivity.kt
@@ -0,0 +1,50 @@
+package douglasspgyn.com.github.maxapp.ui.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import douglasspgyn.com.github.maxapp.R
+import douglasspgyn.com.github.maxapp.extension.isNetworkConnected
+import douglasspgyn.com.github.maxapp.ui.client.ClientActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        clients.setOnClickListener {
+            startActivity(Intent(this, ClientActivity::class.java))
+        }
+
+        orders.setOnClickListener {
+
+        }
+
+        ordersSummary.setOnClickListener {
+
+        }
+
+        tools.setOnClickListener {
+
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        populateFooter()
+    }
+
+    private fun populateFooter() {
+        val pInfo = packageManager.getPackageInfo(packageName, 0)
+        appVersion.text = getString(R.string.app_version, pInfo.versionName)
+
+        if (isNetworkConnected()) {
+            connectionState.setImageDrawable(getDrawable(R.drawable.ic_maxima_nuvem_conectado))
+        } else {
+            connectionState.setImageDrawable(getDrawable(R.drawable.ic_maxima_nuvem_desconectado))
+        }
+    }
+}
